98769aa21e03750e3d8deee45b6bc748acc64db7
Dantalion <88959486+Alexei983@users.noreply.github.com>
2025-05-08 19:29:27 +0000

    Add files via upload

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(
+        '',
+        '',
+        '',
+        {
+                host: 'localhost',
+                dialect: 'postgres'     
+        }
+)
+
+module.exports = sequelize
